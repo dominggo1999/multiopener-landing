5a87df83bae429c1d9e39a348c48e34ae9afcf33
dominggo1999 <arnolddominggo19@gmail.com>
2022-06-20 21:15:44 +0700

    adjust video player style

--- src/common/DemoVideo/DemoVideo.style.jsx
+++ src/common/DemoVideo/DemoVideo.style.jsx
@@ -1,4 +1,4 @@
-import tw, { styled } from 'twin.macro';
+import tw, { styled, theme } from 'twin.macro';
 
 export const VideoWrapper = styled.div`
   ${tw` 
@@ -11,50 +11,6 @@ export const VideoWrapper = styled.div`
   ${({ slug }) => slug && tw`
   mb-6`}
 
-  /* Background */
-  & > .video-react {
-    ${tw`
-      bg-white
-      dark:bg-primary
-      h-full
-    `}
-
-    padding-top : 0!important;
-  }
-
-  /* Play button */
-  .video-react-button.video-react-big-play-button{
-    ${tw`
-      opacity-90
-      hover:(opacity-100 scale-[1.15])
-      -translate-x-1/2 
-      -translate-y-1/2 
-      top-1/2 
-      left-1/2 
-      rounded-full 
-      bg-accent 
-      w-[70px]
-      h-[70px]
-      border-none
-    `}
-
-    transition: transform ease-in-out 180ms;
-
-    &::before{
-      ${tw`
-        hidden
-      `}
-    }
-
-    &::after {
-      ${tw`
-        absolute 
-        -translate-x-1/2 
-        -translate-y-1/2 
-        left-1/2 
-        top-[57%]
-      `}
-      content : url("/icons/video-play-button.svg");
-    }
-  }
+  /* Video player */
+  --plyr-color-main : ${theme`colors.accent`} ;
 `;
